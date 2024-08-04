6fc97772a9d6f4ff8a9cbab3294b23670026499c
Gigi2303 <ajibola.grace201@gmail.com>
2024-08-04 23:33:46 +0100

    Add gh-pages deployment configuration

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <header>
         <div className="App-header">
         <img src={logo} alt="logo" />
         <h1>School dashboard</h1>
       </div>
+      </header>
       <div className="App-body">
         <p>
           Login to access the full dashboard
