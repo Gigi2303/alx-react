f6631533bfc4f27b37f3481b5c52c664e050b617
Gigi2303 <ajibola.grace201@gmail.com>
2024-08-05 01:40:30 +0100

    Reorganized project structure and updated Webpack config

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import Notifications from './Notifications/Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root-notifications')
+);
